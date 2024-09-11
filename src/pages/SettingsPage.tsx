1bd2c1ffb11026beb5c699db79e9c2df6de27712
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-09-11 15:16:56 +0100

    Add farmOS sign in form

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,7 +1,40 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonItem, IonInput } from '@ionic/react';
 import './Page.css';
+import { useForm } from 'react-hook-form';
+import React from "react";
+
 
 const SettingsPage: React.FC = () => {
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      username: '',
+      password: ''
+
+    }
+  });
+
+  //console.log(errors);
+  //console.log(getValues());
+
+function authenticate(data: any) {
+  console.log(data)
+}
+
+
+  /**
+   *
+   * @param data
+   */
+  const onSubmit = (data: any) => {
+    //alert(JSON.stringify(data, null, 2));
+    authenticate(data)
+  };
   
   return (
     <IonPage>
@@ -14,11 +47,33 @@ const SettingsPage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent fullscreen>
-        <>
-            <h1>Settings Page Content</h1>
-        </>
-      </IonContent>
+      <IonContent className="ion-padding">
+        <div id = "container">
+          <h2>farmOS Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+
+            <IonItem>
+              <IonInput label='Username:'
+                {...register('username', {
+                  required: 'This is a required field',
+                })}
+              />
+            </IonItem>
+            <IonItem>
+              <IonInput type='password' label='Password:'
+                {...register('password', {
+                  required: 'This is a required field',
+                })}
+              />
+            </IonItem>
+
+
+            <div>
+              <IonButton type="submit">submit</IonButton>
+            </div>
+          </form>
+          </div>
+        </IonContent>
     </IonPage>
   );
 };
