fa76cb2ef4a06f293d7dc8c31d774aefb6fa2843
Farmer Ed <118817935+AgrinetEd@users.noreply.github.com>
2024-12-02 19:14:47 +0000

    fix automatic oauth token refresh

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,28 +1,24 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Page.css';
 import { useEffect, useState} from "react";
-import { Database, Storage } from '@ionic/storage';
+import { Storage } from '@ionic/storage';
 import {LoginForm} from '../components/LoginForm'
 
 
 //form variables
 const SettingsPage: React.FC = () => {
-  //const [db, setDb] = useState<Database | null>(null);
   const [url, setUrl] = useState(null);
-  //const [store, setStore] = useState<Storage>();
 
   useEffect(() => {
     const setupStorage = async () => {
       const newStore = new Storage();
       const store = await newStore.create();
-      //setStore(store);
  
       const saveUrl = (await store.get('url'));
-      setUrl(saveUrl);
+      setUrl(saveUrl || "https://");
     }
     setupStorage()
   }, [])
-
 
   
   return url ? (
@@ -42,7 +38,7 @@ const SettingsPage: React.FC = () => {
           </div>
         </IonContent>
     </IonPage>
-  ) : <>laoding......</>;
+  ) : <>Loading....</>;
 };
 
 
