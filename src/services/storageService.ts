9e52b7b058c089b9aff0003833ee98837ca24b7c
Farmer Ed <emaherx@gmail.com>
2024-12-11 19:40:24 +0000

    Configure storage service

---
+++ src/services/storageService.ts
@@ -0,0 +1,29 @@
+import { Storage } from '@ionic/storage';
+
+class StorageService {
+  private store: Storage | null = null;
+
+  constructor() {
+    this.init();
+  }
+
+  async init() {
+    const newStore = new Storage();
+    this.store = await newStore.create();
+  }
+
+  async setItem(key: string, value: any) {
+    await this.store?.set(key, value);
+  }
+
+  async getItem(key: string): Promise<any> {
+    return await this.store?.get(key);
+  }
+
+  async removeItem(key: string) {
+    await this.store?.remove(key);
+  }
+}
+
+const storageService = new StorageService();
+export default storageService;
