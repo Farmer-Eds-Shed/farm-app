c9cf0c99de395c2dc28db3057435f775b2d5139d
Farmer Ed <emaherx@gmail.com>
2024-12-11 21:44:32 +0000

    Updates

--- assets/ios.transition-legacy-AOv1lPIB.js
+++ assets/ios.transition-legacy-AOv1lPIB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CEzH7VD4.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-83pZICfX.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
