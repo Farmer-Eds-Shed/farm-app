09ba2691a64e27e6826573eeb5d24803ed914837
Farmer Ed <emaherx@gmail.com>
2024-12-12 07:28:42 +0000

    Updates

--- assets/ios.transition-legacy-BN_N-p-q.js
+++ assets/ios.transition-legacy-BN_N-p-q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DqomrVFU.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DOgZYUYl.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
