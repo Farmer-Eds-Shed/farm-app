4be3ff042f03fd7e33148f1904e24cc9c189c9a3
Farmer Ed <emaherx@gmail.com>
2024-12-21 10:43:23 +0000

    Update server.cjs to handle refresh

--- server.cjs
+++ server.cjs
@@ -16,7 +16,13 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static('www'));
+
+
+app.use(express.static( 'dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
