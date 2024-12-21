14f08ff475cd2b73e06074fe5ebb2b69c2ca20f7
Farmer Ed <emaherx@gmail.com>
2024-12-21 16:16:14 +0000

    Updates

--- assets/ios.transition-legacy-DQnMlHwt.js
+++ assets/ios.transition-legacy-DQnMlHwt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CwLMZRIw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BBpLBifa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
