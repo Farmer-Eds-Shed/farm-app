e902884ceb6a51cc62f8ef8376cbb1664075da2b
Farmer Ed <emaherx@gmail.com>
2025-01-12 20:27:38 +0000

    Updates

--- assets/ios.transition-legacy-CRKZFduM.js
+++ assets/ios.transition-legacy-CRKZFduM.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DxvQejUA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BmUpVBLa.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
